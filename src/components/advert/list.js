36de15e3f850f11dab5185b2ba041abdbeccc180
oktay <oktaycolakoglu@gmail.com>
2021-03-15 21:46:14 +0300

    feat: advert list empty state

--- src/components/advert/list.js
+++ src/components/advert/list.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { Grid } from "@chakra-ui/react";
+import { Flex, Grid, Stack, Text } from "@chakra-ui/react";
 import AdvertCard from "./card";
+import { FiInfo } from 'react-icons/fi';
 
 function AdvertList({ adverts }) {
-  return (
-    <Grid templateColumns={{ base: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap="12">
+  return adverts.length ? (
+    <Grid
+      templateColumns={{
+        base: "1fr",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(3, 1fr)",
+      }}
+      gap="12"
+    >
       {adverts.map((advert) => (
         <AdvertCard key={advert.id} advert={advert} />
       ))}
     </Grid>
+  ) : (
+    <Stack py="36" border="2px dashed" borderColor="gray.200" borderRadius="lg" alignItems="center">
+      <Flex color="gray.300">
+        <FiInfo fontSize="36px" />
+      </Flex>
+      <Text color="gray.300" maxWidth="container.sm" textAlign="center">İlan yok daha sonra tekrar ziyaret ediniz</Text>
+    </Stack>
   );
 }
 
